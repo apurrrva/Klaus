17c9cc26b7aa99f4b8c803014cc8793e51b1192b
vanisha-droid <vanisha.241204@gmail.com>
2025-05-23 17:52:25 +1000

    Update base.py

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -76,10 +76,15 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 
