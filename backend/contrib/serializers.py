dbac14137cffcc7001e59760c81b0a253a4cd03b
vanisha-droid <vanisha.241204@gmail.com>
2025-05-23 13:25:59 +1000

    Update serializers.py

--- backend/contrib/serializers.py
+++ backend/contrib/serializers.py
@@ -1,3 +1,17 @@
 from rest_framework import serializers
+from .models import UserProfile, Swipe, Recommendation
 
-# Create your serializers here.
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['user_id', 'name', 'age', 'gender', 'keywords', 'budget_min', 'budget_max']
+
+class SwipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Swipe
+        fields = ['user', 'product_id', 'status']
+
+class RecommendationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recommendation
+        fields = ['product_id', 'score']
