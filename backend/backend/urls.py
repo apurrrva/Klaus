6ab4a36fff4b24734aa12aeaa7b6983bc4774438
vanisha-droid <vanisha.241204@gmail.com>
2025-05-23 18:17:09 +1000

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import UserCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('contrib.urls')),
+    path('api/users/', UserCreateView.as_view()),
 ]
