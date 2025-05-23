997b40a036ddc0955047e9229968daa9ee71e034
vanisha-droid <vanisha.241204@gmail.com>
2025-05-24 00:31:28 +1000

    Update urls.py

--- backend/contrib/urls.py
+++ backend/contrib/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_products, create_user_profile, record_swipe, get_recommendations
+from contrib.views import get_products, create_user_profile, record_swipe, get_recommendations
 
 urlpatterns = [
     path('products/', get_products, name='get_products'),
