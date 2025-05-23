bda0050d78fc8178a6ecc5d1871ac695992cfa27
vanisha-droid <vanisha.241204@gmail.com>
2025-05-23 14:15:09 +1000

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_products, record_swipe, get_recommendations
+from .views import get_products, create_user_profile, record_swipe, get_recommendations
 
 urlpatterns = [
     path('products/', get_products, name='get_products'),
+    path('users/', create_user_profile, name='create_user_profile'),
     path('swipes/', record_swipe, name='record_swipe'),
     path('recommendations/<str:user_id>/', get_recommendations, name='get_recommendations'),
 ]
