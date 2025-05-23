32bb4595d31b46710f7c5ecb394d39e770aee849
vanisha-droid <vanisha.241204@gmail.com>
2025-05-23 14:09:46 +1000

    Update models.py

--- backend/contrib/models.py
+++ backend/contrib/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-#Mock models for profiles, swipe and recommendation (no products here) (main data in fakestoreapi)
-
 class UserProfile(models.Model):
     user_id = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     gender = models.CharField(max_length=50, blank=True)
-    keywords = models.JSONField()  # List of interests, e.g., ["books", "tech"]
+    keywords = models.JSONField()  # e.g., ["electronics", "books"]
     budget_min = models.DecimalField(max_digits=10, decimal_places=2)
     budget_max = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
